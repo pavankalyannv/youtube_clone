62f027170d5b2e2373c7ef6d2c3e8dfc3d610b3a
Pavan Kalyan <pavankalyan.nv@gmail.com>
2023-07-28 10:52:50 +0530

    sidebar styles modified

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,12 +4,12 @@ import { SIDEBARDATA, SIDEBARDATA1} from "../utils/constants";
 const Sidebar = () => {
 
   return (
-    <div className="m-2 p-5">
-      <ul className="py-2">
+    <div className="m-2 p-5 w-max">
+      <ul className="w-full py-2">
         {SIDEBARDATA.map((i) => (
           <li className="m-1 cursor-pointer hover:bg-gray-100 px-3 rounded-lg">
             <span className="material-symbols-outlined ">{i.icon}</span>
-            <span className="text-md px-1">{i.tabName}</span>
+            <span className="text-md pl-1">{i.tabName}</span>
           </li>
         ))}
       </ul>
